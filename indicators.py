071135defcaad35f1b84fa8d081c24d3ac92e1ce
idrish12 <117757375+idrish12@users.noreply.github.com>
2026-05-03 22:02:23 +0530

    Update indicators.py

--- indicators.py
+++ indicators.py
@@ -9,3 +9,7 @@ def calculate_ema(data, period=14):
 """Calculates the Exponential Moving Average"""
 
     return data['close'].ewm(span=period, adjust=False).mean()
+
+def calculate_rsi(data, period=14):
+
+    """Calculates the Relative Strength Index"""
