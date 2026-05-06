11cbe3a1a2c331807d545de0be88e420859fc1ad
idrish12 <117757375+idrish12@users.noreply.github.com>
2026-05-06 12:34:40 +0530

    Update api_connector.py

--- api_connector.py
+++ api_connector.py
@@ -7,3 +7,7 @@ import time
 # Secure API Connection Manager for Crypto Exchanges
 
 def generate_signature(api_secret, query_string):
+
+"""Generates a cryptographic hash for private API endpoints"""
+
+signature = hmac.new(api_secret.encode('utf-8'), query_string.encode('utf-8'), hashlib.sha256).hexdigest()
