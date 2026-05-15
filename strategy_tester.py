56f57f4986355ac766c246c6c283cca342d03c0c
idrish12 <117757375+idrish12@users.noreply.github.com>
2026-05-15 20:08:08 +0530

    Update strategy_tester.py

--- strategy_tester.py
+++ strategy_tester.py
@@ -11,3 +11,7 @@ def __init__(self, trading_pair="BTC/USDT"):
 self.pair = trading_pair
 
         self.is_running = False
+
+self.risk_module = RiskManager(1000)
+
+    def load_historical_data(self):
